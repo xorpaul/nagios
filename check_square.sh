126e0d8cf9e78fd59542839db9124b6d7d7faf60
Andreas Paul <xorpaul@gmail.com>
2011-11-19 19:50:40 +0100

    fixed grep regex

--- check_square.sh
+++ check_square.sh
@@ -1,6 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
-curl -s -I square.jobs | grep -q ^Location: https://squareup.com/jobs$'\r'
+# Submit a HEAD request to get the response headers
+# then grep for the Location HTTP header to ensure 
+# that the response is a redirect and that the target is correct
+
+curl -s -I square.jobs | grep -q ^Location:\ https://squareup.com/jobs$'\r'
 
 if [ $? -eq 0 ]; then
     echo "OK - http://square.jobs redirects to https://squareup.com/jobs"
@@ -10,20 +14,13 @@ else
     exit 2
 fi
      
-
-
-if [ curl -s -I square.jobs | grep -q ^Location: https://squareup.com/jobs$'\r' ]; then
-    echo "OK - http://square.jobs redirects to https://squareup.com/jobs"
-    exit 0
-else
-    echo "CRITICAL - http://square.jobs DOES NOT redirect to https://squareup.com/jobs"
-    exit 2
-fi
 #set -o pipefail
-#/usr/lib/nagios/plugins/check_http -H square.jobs -N -e "HTTP/1.1 301 Moved Permanently" -v | grep "Location: https://squareup.com/jobs" > /dev/null
+#/usr/lib/nagios/plugins/check_http -H web.de -N -e "HTTP/1.1 301 Moved Permanently" -v | grep -q "Location: https://squareup.com/jobs"
 #
 #if [ $? -eq 0 ]; then
-#    echo "OK"
+#    echo "OK - http://square.jobs redirects to https://squareup.com/jobs"
+#    exit 0
 #else
-#    echo "CRITICAL"
+#    echo "CRITICAL - http://square.jobs DOES NOT redirect to https://squareup.com/jobs"
+#    exit 2
 #fi
